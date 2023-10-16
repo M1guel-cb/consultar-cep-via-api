e5d7b8f969d124f9666ec3e99cbfd21d2f975009
M1guel-cb <132688685+M1guel-cb@users.noreply.github.com>
2023-10-16 17:47:26 -0300

    ok, vamo ver

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -5,6 +5,7 @@ const complemento = document.querySelector("#compl");
 const bairro = document.querySelector("#bairro");
 const botao = document.querySelector("#botao");
 var cep = 0;
+var text
 
 function consultarCEP() {
     cep = document.querySelector("input#cep").value;
@@ -18,17 +19,55 @@ function consultarCEP() {
 
     fetch(url).then(function (response) {
         response.json().then(function (data) {
-            estado.value = data.uf;
-            cidade.value = data.localidade;
-            bairro.value = data.bairro;
-            complemento.value = data.complemento;
-            logradouro.value = data.logradouro;
+            console.log(data)
+            if (data.uf == undefined) {
+                estado.value = 'Não encontrei nada'
+            }
+            else {
+                estado.value = data.uf;
+            }
+
+            if (data.localidade == undefined) {
+                cidade.value = 'Não encontrei nada'
+            }else {
+                cidade.value = data.localidade;
+            }
+
+            if (data.bairro == undefined) {
+                bairro.value = 'Não encontrei nada'
+            }
+            else {
+                bairro.value = data.bairro;
+            }
+
+            if (data.complemento == '') {
+                complemento.value = 'Não possui'
+            }
+            else if (data.complemento == undefined) {
+                complemento.value = "Não encontrei nada";
+            } else {
+                complemento.value = data.complemento;
+            }
+            if (data.logradouro == undefined) {
+                logradouro.value = "Não encontrei nada";
+            }
+            else {
+                logradouro.value = data.logradouro;
+            }
+            if (logradouro.value == 'Não encontrei nada' && complemento.value == 'Não encontrei nada' && bairro.value == 'Não encontrei nada') {
+                text = `CEP inválido!`
+            } else {
+            text = `CEP: ${cep}
+Logradouro: ${logradouro.value}
+Bairro: ${bairro.value}
+Complemento: ${complemento.value}
+Cidade: ${cidade.value}
+Estado: ${estado.value}`
+            }
         });
     });
 }
 
 function copy() {
-    navigator.clipboard.writeText(cep).then(() => {
-        console.log('CEP copiado!');
-    });
+    navigator.clipboard.writeText(text);
 }
